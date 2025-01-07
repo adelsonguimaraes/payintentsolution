380d6138316d20d1dc94fc5b9cf12b310b477fb3
adelsonguimaraes <adelsonguimaraes@gmail.com>
2025-01-07 16:37:04 -0400

    feat(#0000): add documentation paytests

--- PayIntentAPI/Settings/AwsSettings.cs
+++ PayIntentAPI/Settings/AwsSettings.cs
@@ -3,6 +3,6 @@ namespace PaymentIntentAPI.Settings
     public class AwsSettings
     {
         public string? Region { get; set; }
-        public String? SqsUrl { get; set; }
+        public string? SqsUrl { get; set; }
     }
 }
--- PayTests/Controllers/PaymentControllerTests.cs
+++ PayTests/Controllers/PaymentControllerTests.cs
@@ -1,8 +1,4 @@
 using Moq;
-using PayIntentAPI.Controllers;
-using PayIntentAPI.Models;
-using PayIntentAPI.Settings;
-using PayIntentAPI.DTOs;
 using Amazon.SQS;
 using Amazon.SQS.Model;
 using Microsoft.AspNetCore.Mvc;
@@ -15,31 +11,23 @@ using System.Net;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
+using PayIntentAPI.Settings;
 
-namespace PayIntentAPI.Tests
+namespace PayTests
 {
     public class PaymentControllerTests
     {
         private readonly Mock<IAmazonSQS> _mockSqsClient;
-        private readonly Mock<GrpcChannel> _mockGrpcChannel;
-        private readonly Mock<PaymentService.PaymentServiceClient> _mockGrpcClient;
-        private readonly Mock<IOptions<AwsSettings>> _mockAwsSettings;
-        private readonly PaymentController _controller;
+        private readonly IOptions<AwsSettings> _awsSettings;
 
         public PaymentControllerTests()
         {
             _mockSqsClient = new Mock<IAmazonSQS>();
-            _mockAwsSettings = new Mock<IOptions<AwsSettings>>();
-            _mockAwsSettings.Setup(x => x.Value).Returns(new AwsSettings { SqsUrl = "https://example.com/sqs" });
-
-            _mockGrpcChannel = new Mock<GrpcChannel>();
-            _mockGrpcClient = new Mock<PaymentService.PaymentServiceClient>(_mockGrpcChannel.Object);
-
-            _controller = new PaymentController(_mockSqsClient.Object, _mockAwsSettings.Object);
+            _awsSettings = Options.Create(new AwsSettings { SqsUrl = "https://sqs.fakeurl.com" });
         }
 
         [Fact]
-        public async Task CreatePaymentIntent_ShouldReturnOk_WhenMessageSentToSQSAndGrpcCallIsSuccessful()
+        public async Task CreatePaymentIntent_ReturnsOk()
         {
             // Arrange
             var payment = new PaymentModel
@@ -50,105 +38,19 @@ namespace PayIntentAPI.Tests
                 Status = "Pending"
             };
 
-            var sqsResponse = new SendMessageResponse
-            {
-                HttpStatusCode = HttpStatusCode.OK
-            };
+            _mockSqsClient.Setup(x => x.SendMessageAsync(It.IsAny<SendMessageRequest>(), default))
+                          .ReturnsAsync(new SendMessageResponse { HttpStatusCode = System.Net.HttpStatusCode.OK });
 
-            _mockSqsClient.Setup(s => s.SendMessageAsync(It.IsAny<SendMessageRequest>(), default))
-                .ReturnsAsync(sqsResponse);
-
-            var grpcResponse = new PaymentDetails
-            {
-                Uuid = payment.Uuid.ToString(),
-                Amount = payment.Amount.ToString(),
-                Status = "Processed"
-            };
-
-            _mockGrpcClient.Setup(c => c.GetPaymentDetailsAsync(It.IsAny<PaymentRequest>(), default))
-                .ReturnsAsync(grpcResponse);
+            var controller = new PaymentController(_mockSqsClient.Object, _awsSettings);
 
             // Act
-            var result = await _controller.CreatePaymentIntent(payment);
+            var result = await controller.CreatePaymentIntent(payment);
 
             // Assert
-            var okResult = result as OkObjectResult;
-            okResult.Should().NotBeNull();
-            okResult.StatusCode.Should().Be(200);
-
-            var responseValue = okResult.Value as JsonElement?;
-            responseValue.HasValue.Should().BeTrue();
-            responseValue.Value.GetProperty("uuid").GetString().Should().Be(payment.Uuid.ToString());
-        }
-
-        [Fact]
-        public async Task CreatePaymentIntent_ShouldReturnBadRequest_WhenModelStateIsInvalid()
-        {
-            // Arrange
-            var payment = new PaymentModel(); // Invalid model, missing required properties
-            _controller.ModelState.AddModelError("Amount", "Amount is required");
-
-            // Act
-            var result = await _controller.CreatePaymentIntent(payment);
-
-            // Assert
-            var badRequestResult = result as BadRequestObjectResult;
-            badRequestResult.Should().NotBeNull();
-            badRequestResult.StatusCode.Should().Be(400);
-        }
-
-        [Fact]
-        public async Task CreatePaymentIntent_ShouldReturnInternalServerError_WhenExceptionOccurs()
-        {
-            // Arrange
-            var payment = new PaymentModel
-            {
-                Uuid = Guid.NewGuid(),
-                Amount = 100,
-                Currency = "USD",
-                Status = "Pending"
-            };
-
-            _mockSqsClient.Setup(s => s.SendMessageAsync(It.IsAny<SendMessageRequest>(), default))
-                .ThrowsAsync(new Exception("SQS service error"));
-
-            // Act
-            var result = await _controller.CreatePaymentIntent(payment);
-
-            // Assert
-            var internalServerErrorResult = result as ObjectResult;
-            internalServerErrorResult.Should().NotBeNull();
-            internalServerErrorResult.StatusCode.Should().Be(500);
-            internalServerErrorResult.Value.ToString().Should().Contain("SQS service error");
-        }
-
-        [Fact]
-        public async Task CreatePaymentIntent_ShouldReturnStatusCode_WhenSQSMessageFails()
-        {
-            // Arrange
-            var payment = new PaymentModel
-            {
-                Uuid = Guid.NewGuid(),
-                Amount = 100,
-                Currency = "USD",
-                Status = "Pending"
-            };
-
-            var sqsResponse = new SendMessageResponse
-            {
-                HttpStatusCode = HttpStatusCode.BadRequest // Simulating failure to send message to SQS
-            };
-
-            _mockSqsClient.Setup(s => s.SendMessageAsync(It.IsAny<SendMessageRequest>(), default))
-                .ReturnsAsync(sqsResponse);
-
-            // Act
-            var result = await _controller.CreatePaymentIntent(payment);
-
-            // Assert
-            var statusCodeResult = result as StatusCodeResult;
-            statusCodeResult.Should().NotBeNull();
-            statusCodeResult.StatusCode.Should().Be((int)HttpStatusCode.BadRequest);
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<dynamic>(okResult.Value);
+            Assert.Equal(payment.Uuid, returnValue.uuid);
         }
     }
+
 }
